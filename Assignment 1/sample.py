fd573f539796ceb9fe61ebffb66f645566b097ad
abner <b10531017@mail.ntust.edu.tw>
2020-04-16 21:40:50 +0800

    adagrad_sample_based line
    4/16

--- Assignment 1/sample.py
+++ Assignment 1/sample.py
@@ -47,9 +47,78 @@ for month in range(12):
 
 # In[4]:
 # Normalization
-mean = np.mean(x, axis = 0) 
-std = np.std(x, axis = 0)
-for i in range(x.shape[0]):
-    for j in range(x.shape[1]):
-        if not std[j] == 0 :
-            x[i][j] = (x[i][j]- mean[j]) / std[j]
+mean_x = np.mean(x, axis = 0) #18 * 9 
+std_x = np.std(x, axis = 0) #18 * 9 
+for i in range(len(x)): #12 * 471
+    for j in range(len(x[0])): #18 * 9 
+        if std_x[j] != 0:
+            x[i][j] = (x[i][j] - mean_x[j]) / std_x[j]
+x
+
+# In[5]:
+import math
+x_train_set = x[: math.floor(len(x) * 0.8), :]
+y_train_set = y[: math.floor(len(y) * 0.8), :]
+x_validation = x[math.floor(len(x) * 0.8): , :]
+y_validation = y[math.floor(len(y) * 0.8): , :]
+print(x_train_set)
+print(y_train_set)
+print(x_validation)
+print(y_validation)
+print(len(x_train_set))
+print(len(y_train_set))
+print(len(x_validation))
+print(len(y_validation))
+
+# In[6]:
+# Training
+dim = 18 * 9 + 1
+w = np.zeros([dim, 1])
+x = np.concatenate((np.ones([12 * 471, 1]), x), axis = 1).astype(float)
+learning_rate = 100
+iter_time = 1000
+adagrad = np.zeros([dim, 1])
+eps = 0.0000000001
+for t in range(iter_time):
+    loss = np.sqrt(np.sum(np.power(np.dot(x, w) - y, 2))/471/12)#rmse
+    if(t%100==0):
+        print(str(t) + ":" + str(loss))
+    gradient = 2 * np.dot(x.transpose(), np.dot(x, w) - y) #dim*1
+    adagrad += gradient ** 2
+    w = w - learning_rate * gradient / np.sqrt(adagrad + eps)
+np.save('weight.npy', w)
+w
+
+# In[7]:
+# Testing
+testdata = pd.read_csv('data/test.csv', header = None, encoding = 'big5')
+test_data = testdata.iloc[:, 2:]
+test_data[test_data == 'NR'] = 0
+test_data = test_data.to_numpy()
+test_x = np.empty([240, 18*9], dtype = float)
+for i in range(240):
+    test_x[i, :] = test_data[18 * i: 18* (i + 1), :].reshape(1, -1)
+for i in range(len(test_x)):
+    for j in range(len(test_x[0])):
+        if std_x[j] != 0:
+            test_x[i][j] = (test_x[i][j] - mean_x[j]) / std_x[j]
+test_x = np.concatenate((np.ones([240, 1]), test_x), axis = 1).astype(float)
+test_x
+
+# In[8]:
+# Prediction
+w = np.load('weight.npy')
+ans_y = np.dot(test_x, w)
+ans_y
+
+# In[9]:
+# Save Prediction to CSV File
+with open('submit.csv', mode='w', newline='') as submit_file:
+    csv_writer = csv.writer(submit_file)
+    header = ['id', 'value']
+    print(header)
+    csv_writer.writerow(header)
+    for i in range(240):
+        row = ['id_' + str(i), ans_y[i][0]]
+        csv_writer.writerow(row)
+        print(row)
