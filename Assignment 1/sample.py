8b36c9cfd6d8e43f4bc722c7ed5cac94501b3807
abner <b10531017@mail.ntust.edu.tw>
2020-04-17 17:40:44 +0800

    4/17

--- Assignment 1/sample.py
+++ Assignment 1/sample.py
@@ -110,17 +110,3 @@ test_x
 w = np.load('weight.npy')
 ans_y = np.dot(test_x, w)
 ans_y
-
-'''
-# In[9]:
-# Save Prediction to CSV File
-with open('submit.csv', mode='w', newline='') as submit_file:
-    csv_writer = csv.writer(submit_file)
-    header = ['id', 'value']
-    print(header)
-    csv_writer.writerow(header)
-    for i in range(240):
-        row = ['id_' + str(i), ans_y[i][0]]
-        csv_writer.writerow(row)
-        print(row)
-'''
