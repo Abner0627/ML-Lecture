b3ed06d689d8a60b77b8cb621eeaad49e64c2ed4
abner <b10531017@mail.ntust.edu.tw>
2020-04-19 23:00:05 +0800

    4/19
    finish In[3]
    rewrite In[2]

--- Assignment 1/sample.py
+++ Assignment 1/sample.py
@@ -23,28 +23,34 @@ data = raw_data[:,3:]
 # 取其第3個column之後的數值
 data[data=="NR"] = 0
 # 將NaN定為0
-month_to_data = {}  ## Dictionary (key:month , value:data)
+month_data = {}  ## Dictionary (key:month , value:data)
 for month in range(12):
-    sample = np.empty(shape = (18 , 480))
+    sample = np.empty([18, 480])
     # 每個月18個空氣成分共有24*20筆data，故建立一18*480的array
     for day in range(20):
-        for hour in range(24): 
-            sample[:,day * 24 + hour] = data[18 * (month * 20 + day): 18 * (month * 20 + day + 1),hour]
-            # 於sample放入第day天第hour小時的18筆空氣成分data
-    month_to_data[month] = sample
+        sample[:, day * 24 : (day + 1) * 24] = data[18 * (20 * month + day) : 18 * (20 * month + day + 1), :]
+        # 於sample放入第month月第day天的18筆空氣成分24小時的data
+    month_data[month] = sample
 
 # In[3]:
 # Preprocess
 x = np.empty(shape = (12 * 471 , 18 * 9),dtype = float)
+# 宣告x為前9個小時18個空氣成分的data
+# 每個月會有480個小時，每9小時形成一個data(1~9 2~10 3~11...471~480)，每個月會有471個data
 y = np.empty(shape = (12 * 471 , 1),dtype = float)
+# 宣告y為前9個小時PM2.5的data
 
 for month in range(12): 
     for day in range(20): 
         for hour in range(24):   
             if day == 19 and hour > 14:
-                continue  
-            x[month * 471 + day * 24 + hour,:] = month_to_data[month][:,day * 24 + hour : day * 24 + hour + 9].reshape(1,-1) 
-            y[month * 471 + day * 24 + hour,0] = month_to_data[month][9 ,day * 24 + hour + 9]
+                continue
+                # 跳過第20天第15小時(不含)以後的data(超出471)
+            x[month * 471 + day * 24 + hour,:] = month_data[month][:,day * 24 + hour : day * 24 + hour + 9].reshape(1,-1)
+            # 將第month月第day天第1~9、2~10、3~11...15~24小時的data放入x
+            # reshape(1,-1):使每次取出的data(18*9)變為(1*162)    參見:https://blog.csdn.net/W_weiying/article/details/82112337
+            y[month * 471 + day * 24 + hour,0] = month_data[month][9 ,day * 24 + hour + 9]
+            # 將第month月第day天第10個小時的PM2.5data放入y
 
 # In[4]:
 # Normalization
